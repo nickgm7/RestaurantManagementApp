a70343b917140d4fc2aaab702769e21c33702abb
Nicholas Meyer <46453785+nickgm7@users.noreply.github.com>
2021-11-30 11:32:50 -0700

    started home page and fixed python code

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from send_mail import send_mail
 
 app = Flask(__name__)
 
@@ -38,23 +37,9 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    if request.method == 'POST':
-        customer = request.form['customer']
-        dealer = request.form['dealer']
-        rating = request.form['rating']
-        comments = request.form['comments']
-        # print(customer, dealer, rating, comments)
-        if customer == '' or dealer == '':
-            return render_template('index.html', message='Please enter required fields')
-        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
-            data = Feedback(customer, dealer, rating, comments)
-            db.session.add(data)
-            db.session.commit()
-            send_mail(customer, dealer, rating, comments)
-            return render_template('success.html')
-        return render_template('index.html', message='You have already submitted feedback')
+@app.route('/admin', methods=['POST'])
+def admin():
+    return render_template('admin.html')
 
 
 if __name__ == '__main__':
